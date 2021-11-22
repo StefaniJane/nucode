1eef8191a46e747e52bf29890fe6ddbbd03c4d15
StefaniJane <93358929+StefaniJane@users.noreply.github.com>
2021-11-21 19:44:13 -0500

    NPM Scripts Part 2

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").on("click",function () {
+      //here were checking if this button, when its clicked, is a pause button.
+      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel("pause");
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+      } else {
+        $(".carousel").carousel("cycle"); //here were causing the carousel to cycle again.
+        $("#carouselButton").children("i").removeClass("fa-play"); // here we are removing the play button using the jquery .children & .removeClass methods.
+        $("#carouselButton").children("i").addClass("fa-pause"); // here we are replacing the removed play button w/ the pause icon; using the jquery .children & .addClass methods.
+      }
+    });
+  });
